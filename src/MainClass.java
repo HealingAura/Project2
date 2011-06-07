ed4aaac4e1b38d31c40df17a84b59d8d06a84da9
Lior Kiperman <HealingAura@gmail.com>
2011-06-08 02:56:16 +0300

    Added another line

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,5 +3,6 @@ public class MainClass
     public static void main(String args[])
     {
         System.out.println("Hello World! Hello again");
+        System.out.println("This is my first change");
     }
 }
